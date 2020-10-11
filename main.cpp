db4dfa2801fe1126ef549344bd61ca4606155f0f
Infromatica II <eder.ariza@udea.edu.co>
2020-10-10 22:41:58 -0500

    Ejercicio 20 - Laboratorio 1

--- main.cpp
+++ main.cpp
@@ -5,25 +5,86 @@ Ej: si se ingresa 121 se debe imprimir:
 121 es un numero palindromo.
 y si se ingresa 123 se debe imprimir:
 123 NO es un numero palindromo
+
+            residuo = numero_disminuye%(potencia*10);
+            numero_nuevo += residuo;
+            numero_disminuye = numero - numero_nuevo;
+            potencia *= 10;
+
+            cout << residuo << " "<< numero_nuevo << " " << numero_disminuye << " " << potencia << endl;
+
+        }
+
+        if(numero_nuevo == numero)
+            cout << "Es palindromo" <<  endl;
+        else
+            cout << "No es palindromo" << endl;
 */
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
 
-    int numero_ingresado;
-    int contador = 0;
-    cout << "Ingrese el numero: ";
-    cin >> numero_ingresado;
+        int numero;
+        int digito = 0;
+        int residuo = 0;
 
-    for(int i=1; i<=numero_ingresado; i++) {
+        cout << "Ingrese un numero ";
+        cin >> numero;
 
-        if((numero_ingresado % i) == 0) {
-                contador += 1;
+        //Bloque que descompone el numero hallando sus digitos
+        int numero_nuevo = numero;
+        while (numero_nuevo > 0){
+
+            if (numero_nuevo % 10 != 0)
+                digito += 1,
+                residuo = numero_nuevo % 10,
+                numero_nuevo = numero_nuevo / 10;
+
+        else
+                digito += 1,
+                residuo = numero_nuevo % 10,
+                numero_nuevo = numero_nuevo / 10;
 
         }
-    }
 
-    return 0;
-}
+        //Bloque de intrucciones que halla la mayor potencia de 10 del numero ingresado
+        int potencia;
+        for (int i = 0; i < digito; i++){
+
+            if (i == 0)
+                potencia = 1;
+
+            else
+                potencia *= 10;
+        }
+
+        //Bloque de instrucciones que pone el numero al reves
+        int numero_palindromo = 0;
+        numero_nuevo = numero;
+        while (numero_nuevo > 0){
+
+            if (numero_nuevo % 10 != 0)
+                residuo = numero_nuevo % 10,
+                numero_palindromo += residuo * potencia,
+                numero_nuevo = numero_nuevo / 10,
+                potencia /= 10;
+
+            else
+                residuo = numero_nuevo % 10,
+                numero_palindromo += residuo * potencia,
+                numero_nuevo = numero_nuevo / 10,
+                potencia /= 10;
+
+        }
+
+        if (numero_palindromo == numero)
+            cout << "El numero " << numero << " es palindromo " << endl;
+
+        else
+            cout << "El numero " << numero << " no es palindromo " << endl;
+
+        return 0;
+    }
